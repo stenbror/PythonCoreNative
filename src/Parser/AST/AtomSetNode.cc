1ecbe78de1ba3138e890ad3a180b2311b804a05b
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-25 18:59:51 +0200

    Finished Atom nodes

---
+++ src/Parser/AST/AtomSetNode.cc
@@ -0,0 +1,16 @@
+
+#include <ast/AtomSetNode.h>
+
+using namespace PythonCoreNative::RunTime::Parser::AST;
+
+AtomSetNode::AtomSetNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<NumberToken> op1,
+                            std::shared_ptr<ExpressionNode> right,
+                            std::shared_ptr<NumberToken> op2
+                        ) : ExpressionNode(start, end)
+{
+    mOp1 = op1;
+    mRight = right;
+    mOp2 = op2;
+}
