b6b701af272c018f2db425d5e5f9a570e627eee4
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-25 23:48:07 +0200

    Added call Node

---
+++ src/Parser/AST/CallNode.cc
@@ -0,0 +1,16 @@
+
+#include <ast/CallNode.h>
+
+using namespace PythonCoreNative::RunTime::Parser::AST;
+
+CallNode::CallNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> right,
+                            std::shared_ptr<Token> op2
+                        ) : ExpressionNode(start, end)
+{
+    mOp1 = op1;
+    mRight = right;
+    mOp2 = op2;
+}
