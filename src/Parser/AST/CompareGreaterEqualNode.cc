bd2e2f43e651b79ced5aef670576945e93247abc
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-25 21:58:17 +0200

    Added upto NotEqual Node

---
+++ src/Parser/AST/CompareGreaterEqualNode.cc
@@ -0,0 +1,16 @@
+
+#include <ast/CompareGreaterEqualNode.h>
+
+using namespace PythonCoreNative::RunTime::Parser::AST;
+
+CompareGreaterEqualNode::CompareGreaterEqualNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> right
+                        ) : ExpressionNode(start, end)
+{
+    mLeft = left;
+    mOp1 = op1;
+    mRight = right;
+}
---
+++ src/Parser/AST/CompareNotEqualNode.cc
@@ -0,0 +1,16 @@
+
+#include <ast/CompareNotEqualNode.h>
+
+using namespace PythonCoreNative::RunTime::Parser::AST;
+
+CompareNotEqualNode::CompareNotEqualNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> right
+                        ) : ExpressionNode(start, end)
+{
+    mLeft = left;
+    mOp1 = op1;
+    mRight = right;
+}
