8c5b101d3bd205535fffddcab3d9790bb6760f48
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-25 22:46:47 +0200

    Added AndTest Node

---
+++ src/Parser/AST/AndTestNode.cc
@@ -0,0 +1,16 @@
+
+#include <ast/AndTestNode.h>
+
+using namespace PythonCoreNative::RunTime::Parser::AST;
+
+AndTestNode::AndTestNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> right
+                        ) : ExpressionNode(start, end)
+{
+    mLeft = left;
+    mOp1 = op1;
+    mRight = right;
+}
