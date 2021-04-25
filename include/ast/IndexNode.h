f72b4b800a2044b6435c1da77cd321f89e329f4c
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-25 23:51:01 +0200

    Added Index Node

---
+++ include/ast/IndexNode.h
@@ -0,0 +1,24 @@
+
+#include <ast/ExpressionNode.h>
+#include <Token.h>
+
+#include <memory>
+
+namespace PythonCoreNative::RunTime::Parser::AST
+{
+    class IndexNode : protected ExpressionNode
+    {
+        public:
+            IndexNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> right,
+                            std::shared_ptr<Token> op2
+                        );
+
+        protected:
+            std::shared_ptr<Token> mOp1;
+            std::shared_ptr<ExpressionNode> mRight;
+            std::shared_ptr<Token> mOp2;
+    };
+}
