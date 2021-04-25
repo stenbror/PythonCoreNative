a51e132777be33ce30addd389f051a37d628ade3
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-25 21:27:21 +0200

    Bit Node And, Or, Xor.

---
+++ src/Parser/AST/BitAndNode.cc
@@ -0,0 +1,16 @@
+
+#include <ast/BitAndNode.h>
+
+using namespace PythonCoreNative::RunTime::Parser::AST;
+
+BitAndNode::BitAndNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> right
+                        ) : ExpressionNode(start, end)
+{
+    mOp1 = op1;
+    mLeft = left;
+    mRight = right;
+}
---
+++ src/Parser/AST/BitXorNode.cc
@@ -0,0 +1,16 @@
+
+#include <ast/BitXorNode.h>
+
+using namespace PythonCoreNative::RunTime::Parser::AST;
+
+BitXorNode::BitXorNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> right
+                        ) : ExpressionNode(start, end)
+{
+    mOp1 = op1;
+    mLeft = left;
+    mRight = right;
+}
