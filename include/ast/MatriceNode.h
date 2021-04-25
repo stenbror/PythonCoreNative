c8bc831f3290e3e8cc0e817ea46f739770f61f60
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-25 21:02:57 +0200

    Added Matrice Node

---
+++ include/ast/MatriceNode.h
@@ -0,0 +1,27 @@
+
+#include <ast/ExpressionNode.h>
+#include <Token.h>
+
+#include <memory>
+#include <vector>
+
+
+namespace PythonCoreNative::RunTime::Parser::AST
+{
+    class MatriceNode : protected ExpressionNode
+    {
+        public:
+            MatriceNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<NumberToken> op1,
+                            std::shared_ptr<ExpressionNode> right
+                        );
+
+        protected:
+            std::shared_ptr<ExpressionNode> mLeft;
+            std::shared_ptr<NumberToken> mOp1;
+            std::shared_ptr<ExpressionNode> mRight;
+    };
+}
+
