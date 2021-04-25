f62e61de4b9b9e0238a7fef862ca78787b3ced10
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-25 21:17:04 +0200

    Added Shift Node

---
+++ src/Parser/AST/ShiftLeftNode.cc
@@ -0,0 +1,16 @@
+
+#include <ast/ShiftLeftNode.h>
+
+using namespace PythonCoreNative::RunTime::Parser::AST;
+
+ShiftLeftNode::ShiftLeftNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<NumberToken> op1,
+                            std::shared_ptr<ExpressionNode> right
+                        ) : ExpressionNode(start, end)
+{
+    mOp1 = op1;
+    mLeft = left;
+    mRight = right;
+}
