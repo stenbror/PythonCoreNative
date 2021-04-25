76353ee5a8520410906b468bfba637e4aad84f6e
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-25 20:58:01 +0200

    Added FloorDiv node

---
+++ include/ast/DivNode.h
@@ -0,0 +1,27 @@
+
+#include <ast/ExpressionNode.h>
+#include <Token.h>
+
+#include <memory>
+#include <vector>
+
+
+namespace PythonCoreNative::RunTime::Parser::AST
+{
+    class DivNode : protected ExpressionNode
+    {
+        public:
+            DivNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<NumberToken> op1,
+                            std::shared_ptr<ExpressionNode> right
+                        );
+
+        protected:
+            std::shared_ptr<ExpressionNode> mLeft;
+            std::shared_ptr<NumberToken> mOp1;
+            std::shared_ptr<ExpressionNode> mRight;
+    };
+}
+
