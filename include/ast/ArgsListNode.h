2f510a69d7b3405bb4f9bde5fdb23f45dae9ea8a
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-26 22:15:36 +0200

    Added ArgsList Node

---
+++ include/ast/ArgsListNode.h
@@ -0,0 +1,23 @@
+
+#include <ast/ExpressionNode.h>
+#include <Token.h>
+
+#include <memory>
+#include <vector>
+
+namespace PythonCoreNative::RunTime::Parser::AST
+{
+    class ArgsListNode : protected ExpressionNode
+    {
+        public:
+            ArgsListNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<std::vector<std::shared_ptr<ExpressionNode>>> nodes,
+                            std::shared_ptr<std::vector<std::shared_ptr<Token>>> separators
+                        );
+
+        protected:
+            std::shared_ptr<std::vector<std::shared_ptr<ExpressionNode>>> mNodes;
+            std::shared_ptr<std::vector<std::shared_ptr<Token>>> mSeparators;
+    };
+}
