964061043d1ae93c35cc77b705da90e632a97fd1
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-26 22:43:30 +0200

    Added Argument Node

---
+++ include/ast/ArgumentNode.h
@@ -0,0 +1,25 @@
+
+#include <ast/ExpressionNode.h>
+#include <Token.h>
+
+#include <memory>
+
+namespace PythonCoreNative::RunTime::Parser::AST
+{
+    class ArgumentNode : protected ExpressionNode
+    {
+        public:
+            ArgumentNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> right
+                        );
+
+        protected:
+            std::shared_ptr<ExpressionNode> mLeft;
+            std::shared_ptr<Token> mOp1;
+            std::shared_ptr<ExpressionNode> mRight;
+    };
+}
+
---
+++ src/Parser/AST/ArgumentNode.cc
@@ -0,0 +1,16 @@
+
+#include <ast/ArgumentNode.h>
+
+using namespace PythonCoreNative::RunTime::Parser::AST;
+
+ArgumentNode::ArgumentNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> right
+                        ) : ExpressionNode(start, end)
+{
+    mOp1 = op1;
+    mLeft = left;
+    mRight = right;
+}
