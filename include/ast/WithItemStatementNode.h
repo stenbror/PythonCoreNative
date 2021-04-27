846c2d7bfd2ed446d458d73e3dffcb49852f636d
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-27 21:08:18 +0200

    Added WithItem Statement Node

---
+++ include/ast/WithItemStatementNode.h
@@ -0,0 +1,27 @@
+
+#include <ast/StatementNode.h>
+#include <ast/ExpressionNode.h>
+#include <Token.h>
+
+#include <memory>
+#include <vector>
+
+
+namespace PythonCoreNative::RunTime::Parser::AST
+{
+    class WithItemStatementNode : protected StatementNode
+    {
+        public:
+            WithItemStatementNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> right
+                        );
+
+        protected:
+            std::shared_ptr<ExpressionNode> mLeft;
+            std::shared_ptr<Token> mOp1;
+            std::shared_ptr<ExpressionNode> mRight;
+    };
+}
---
+++ src/Parser/AST/WithItemStatementNode.cc
@@ -0,0 +1,16 @@
+
+#include <ast/WithItemStatementNode.h>
+
+using namespace PythonCoreNative::RunTime::Parser::AST;
+
+WithItemStatementNode::WithItemStatementNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> right
+                        ) : StatementNode(start, end)
+{
+    mLeft = left;
+    mOp1 = op1;
+    mRight = right;
+}
