9e2b768e3382d5f2e6c6a3237d5bd590e280f930
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-28 21:54:57 +0200

    Added ShiftRightAssign Statement Node

---
+++ src/Parser/AST/ShiftRightAssignStatementNode.cc
@@ -0,0 +1,16 @@
+
+#include <ast/ShiftRightAssignStatementNode.h>
+
+using namespace PythonCoreNative::RunTime::Parser::AST;
+
+ShiftRightAssignStatementNode::ShiftRightAssignStatementNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<Node> left,
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> right
+                        ) : StatementNode(start, end)
+{
+    mLeft = left;
+    mOp1 = op1;
+    mRight = right;
+}
