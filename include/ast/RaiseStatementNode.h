6e76fa157d094fc5839c2fcb7437c00b24c71bbd
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-04-29 22:27:38 +0200

    Added RaiseStatement Node

---
+++ include/ast/RaiseStatementNode.h
@@ -0,0 +1,27 @@
+
+#include <ast/StatementNode.h>
+#include <ast/ExpressionNode.h>
+#include <Token.h>
+
+#include <memory>
+
+namespace PythonCoreNative::RunTime::Parser::AST
+{
+    class RaiseStatementNode : protected StatementNode
+    {
+        public:
+            RaiseStatementNode(  
+                            unsigned int start, unsigned int end,
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<Token> op2,
+                            std::shared_ptr<ExpressionNode> right
+                        );
+
+        protected:
+            std::shared_ptr<Token> mOp1;
+            std::shared_ptr<ExpressionNode> mLeft;
+            std::shared_ptr<Token> mOp2;
+            std::shared_ptr<ExpressionNode> mRight;
+    };
+}
---
+++ src/Parser/AST/RaiseStatementNode.cc
@@ -0,0 +1,18 @@
+
+#include <ast/RaiseStatementNode.h>
+
+using namespace PythonCoreNative::RunTime::Parser::AST;
+
+RaiseStatementNode::RaiseStatementNode(  
+                            unsigned int start, unsigned int end, 
+                            std::shared_ptr<Token> op1,
+                            std::shared_ptr<ExpressionNode> left,
+                            std::shared_ptr<Token> op2,
+                            std::shared_ptr<ExpressionNode> right
+                        ) : StatementNode(start, end)
+{
+    mOp1 = op1;
+    mLeft = left;
+    mOp2 = op2;
+    mRight = right;
+}
