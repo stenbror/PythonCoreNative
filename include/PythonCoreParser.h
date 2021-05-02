63dca99df575e923e6fc6fc4a29d6e929a650583
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-02 18:26:34 +0200

    Added template upto NamedExpr

--- include/PythonCoreParser.h
+++ include/PythonCoreParser.h
@@ -17,6 +17,24 @@ namespace PythonCoreNative::RunTime::Parser
 
         protected:
             std::shared_ptr<AST::ExpressionNode> ParseAtom();
+            std::shared_ptr<AST::ExpressionNode> ParseAtomExpr();
+            std::shared_ptr<AST::ExpressionNode> ParsePower();
+            std::shared_ptr<AST::ExpressionNode> ParseFactor();
+            std::shared_ptr<AST::ExpressionNode> ParseTerm();
+            std::shared_ptr<AST::ExpressionNode> ParseArith();
+            std::shared_ptr<AST::ExpressionNode> ParseShift();
+            std::shared_ptr<AST::ExpressionNode> ParseAndExpr();
+            std::shared_ptr<AST::ExpressionNode> ParseXorExpr();
+            std::shared_ptr<AST::ExpressionNode> ParseOrExpr();
+            std::shared_ptr<AST::ExpressionNode> ParseStarExpr();
+            std::shared_ptr<AST::ExpressionNode> ParseComparison();
+            std::shared_ptr<AST::ExpressionNode> ParseNotTest();
+            std::shared_ptr<AST::ExpressionNode> ParseAndTest();
+            std::shared_ptr<AST::ExpressionNode> ParseOrTest();
+            std::shared_ptr<AST::ExpressionNode> ParseLambda(bool isCond);
+            std::shared_ptr<AST::ExpressionNode> ParseTestNoCond();
+            std::shared_ptr<AST::ExpressionNode> ParseTest();
+            std::shared_ptr<AST::ExpressionNode> ParseNamedExpr();
 
         protected:
             std::shared_ptr<PythonCoreTokenizer> mLexer;
--- src/Parser/PythonCoreParserExpressionRules.cc
+++ src/Parser/PythonCoreParserExpressionRules.cc
@@ -6,3 +6,94 @@ std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseAtom()
 {
     return nullptr;
 }
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseAtomExpr()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParsePower()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseFactor()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseTerm()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseArith()
+{
+    return nullptr;
+}
+
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseShift()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseAndExpr()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseXorExpr()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseOrExpr()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseStarExpr()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseComparison()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseNotTest()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseAndTest()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseOrTest()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseLambda(bool isCond)
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseTestNoCond()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseTest()
+{
+    return nullptr;
+}
+
+std::shared_ptr<AST::ExpressionNode> PythonCoreParser::ParseNamedExpr()
+{
+    return nullptr;
+}
