aadd7f4af3856b49ddbbf2a7f57c1e8e3799bbb9
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-11 21:17:21 +0200

    Started on UnitTest linking against project

---
+++ tests/TestPythonCoreEntry.cc
@@ -0,0 +1,41 @@
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+
+#include <PythonCoreParser.h>
+
+using namespace PythonCoreNative::RunTime::Parser;
+
+TEST_CASE( "Dummy Test: ****** Not usefull ******", "[vector]" ) {
+
+    auto lexer = std::make_shared<PythonCoreParser>(nullptr);
+    std::vector<int> v( 5 );
+
+    REQUIRE( v.size() == 5 );
+    REQUIRE( v.capacity() >= 5 );
+
+    SECTION( "resizing bigger changes size and capacity" ) {
+        v.resize( 10 );
+
+        REQUIRE( v.size() == 10 );
+        REQUIRE( v.capacity() >= 10 );
+    }
+    SECTION( "resizing smaller changes size but not capacity" ) {
+        v.resize( 0 );
+
+        REQUIRE( v.size() == 0 );
+        REQUIRE( v.capacity() >= 5 );
+    }
+    SECTION( "reserving bigger changes capacity but not size" ) {
+        v.reserve( 10 );
+
+        REQUIRE( v.size() == 5 );
+        REQUIRE( v.capacity() >= 10 );
+    }
+    SECTION( "reserving smaller does not change size or capacity" ) {
+        v.reserve( 0 );
+
+        REQUIRE( v.size() == 5 );
+        REQUIRE( v.capacity() >= 5 );
+    }
+}
