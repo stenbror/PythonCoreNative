7f0a0e1499a03cf9db22035d7641d2a94f1e749d
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-23 00:35:28 +0200

    BitAndAssign access

--- src/Parser/AST/BitAndAssignStatementNode.cc
+++ src/Parser/AST/BitAndAssignStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/BitAndAssignStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 BitAndAssignStatementNode::BitAndAssignStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ BitAndAssignStatementNode::BitAndAssignStatementNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<Node> BitAndAssignStatementNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> BitAndAssignStatementNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> BitAndAssignStatementNode::GetRight()
+{
+    return mRight;
 }
