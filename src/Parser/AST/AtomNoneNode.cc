4526c565b168b6f02e7fc5c8c3ef0e942fc38ddc
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-22 23:07:31 +0200

    AtomNone access

--- src/Parser/AST/AtomNoneNode.cc
+++ src/Parser/AST/AtomNoneNode.cc
@@ -2,8 +2,14 @@
 #include <ast/AtomNoneNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 AtomNoneNode::AtomNoneNode(unsigned int start, unsigned int end, std::shared_ptr<Token> op1) : ExpressionNode(start, end)
 {
     mOp1 = op1;
+}
+
+std::shared_ptr<Token> AtomNoneNode::GetOperator()
+{
+    return mOp1;
 }
