587aaefb27eb7bcdf5175e48450ffd4e5bab2741
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-23 00:55:57 +0200

    BitXorAssign access

--- src/Parser/AST/BitXorAssignStatementNode.cc
+++ src/Parser/AST/BitXorAssignStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/BitXorAssignStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 BitXorAssignStatementNode::BitXorAssignStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ BitXorAssignStatementNode::BitXorAssignStatementNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<Node> BitXorAssignStatementNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> BitXorAssignStatementNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> BitXorAssignStatementNode::GetRight()
+{
+    return mRight;
 }
