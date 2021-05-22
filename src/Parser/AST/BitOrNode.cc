3937283e32c13fa2e705982a51547458f2747887
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-23 00:48:51 +0200

    BitOr access

--- src/Parser/AST/BitOrNode.cc
+++ src/Parser/AST/BitOrNode.cc
@@ -2,6 +2,7 @@
 #include <ast/BitOrNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 BitOrNode::BitOrNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ BitOrNode::BitOrNode(
     mOp1 = op1;
     mLeft = left;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode> BitOrNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> BitOrNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> BitOrNode::GetRight()
+{
+    return mRight;
 }
