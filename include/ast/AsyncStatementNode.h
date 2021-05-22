b9cac0a1483931ca1dad68daa979e93b3541285b
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-22 19:06:45 +0200

    Async access

--- include/ast/AsyncStatementNode.h
+++ include/ast/AsyncStatementNode.h
@@ -16,6 +16,9 @@ namespace PythonCoreNative::RunTime::Parser::AST
                             std::shared_ptr<StatementNode> right
                         );
 
+            std::shared_ptr<Token> GetOperator();
+            std::shared_ptr<StatementNode> GetRight();
+
         protected:
             std::shared_ptr<Token> mOp1;
             std::shared_ptr<StatementNode> mRight;
--- src/Parser/AST/AsyncStatementNode.cc
+++ src/Parser/AST/AsyncStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/AsyncStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 AsyncStatementNode::AsyncStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -11,4 +12,14 @@ AsyncStatementNode::AsyncStatementNode(
 {
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<Token> AsyncStatementNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<StatementNode> AsyncStatementNode::GetRight()
+{
+    return mRight;
 }
