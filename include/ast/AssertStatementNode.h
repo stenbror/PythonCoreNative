5d1515f4b2b5f51ecc379a78a3fcd627d2fa7038
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-22 18:55:59 +0200

    AssertStatement access

--- include/ast/AssertStatementNode.h
+++ include/ast/AssertStatementNode.h
@@ -16,8 +16,13 @@ namespace PythonCoreNative::RunTime::Parser::AST
                             std::shared_ptr<Token> op1,
                             std::shared_ptr<ExpressionNode> left,
                             std::shared_ptr<Token> op2,
-                            std::shared_ptr<ExpressionNode> right
-                        );
+                            std::shared_ptr<ExpressionNode> right );
+
+            std::shared_ptr<Token> GetOperator1();
+            std::shared_ptr<ExpressionNode> GetLeft();
+            std::shared_ptr<Token> GetOperator2();
+            std::shared_ptr<ExpressionNode> GetRight();
+                    
 
         protected:
             std::shared_ptr<Token> mOp1;
--- src/Parser/AST/AssertStatementNode.cc
+++ src/Parser/AST/AssertStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/AssertStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 AssertStatementNode::AssertStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -15,4 +16,24 @@ AssertStatementNode::AssertStatementNode(
     mLeft = left;
     mOp2 = op2;
     mRight = right;
+}
+
+std::shared_ptr<Token> AssertStatementNode::GetOperator1()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> AssertStatementNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> AssertStatementNode::GetOperator2()
+{
+    return mOp2;
+}
+
+std::shared_ptr<ExpressionNode> AssertStatementNode::GetRight()
+{
+    return mRight;
 }
