6e1012709d52cc918d8bd7c4dd4ca595d8ba3b62
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-23 00:22:56 +0200

    AtomTrue access

--- src/Parser/AST/AtomTrueNode.cc
+++ src/Parser/AST/AtomTrueNode.cc
@@ -2,8 +2,14 @@
 #include <ast/AtomTrueNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 AtomTrueNode::AtomTrueNode(unsigned int start, unsigned int end, std::shared_ptr<Token> op1) : ExpressionNode(start, end)
 {
     mOp1 = op1;
+}
+
+std::shared_ptr<Token> AtomTrueNode::GetOperator()
+{
+    return mOp1;
 }
