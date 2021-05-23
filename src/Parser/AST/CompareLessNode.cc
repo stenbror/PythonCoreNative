1d6a65760815ad51715310dc28a1fe93bfefe4a1
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-23 15:21:51 +0200

    Less access

--- src/Parser/AST/CompareLessNode.cc
+++ src/Parser/AST/CompareLessNode.cc
@@ -2,6 +2,7 @@
 #include <ast/CompareLessNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 CompareLessNode::CompareLessNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ CompareLessNode::CompareLessNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode>  CompareLessNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> CompareLessNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> CompareLessNode::GetRight()
+{
+    return mRight;
 }
