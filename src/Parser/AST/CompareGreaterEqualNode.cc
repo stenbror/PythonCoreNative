3aebb6f2bc6c073317688a4f2073039be3914689
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-23 14:28:54 +0200

    GreaterEqual access

--- src/Parser/AST/CompareGreaterEqualNode.cc
+++ src/Parser/AST/CompareGreaterEqualNode.cc
@@ -2,6 +2,7 @@
 #include <ast/CompareGreaterEqualNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 CompareGreaterEqualNode::CompareGreaterEqualNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ CompareGreaterEqualNode::CompareGreaterEqualNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode>  CompareGreaterEqualNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> CompareGreaterEqualNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> CompareGreaterEqualNode::GetRight()
+{
+    return mRight;
 }
