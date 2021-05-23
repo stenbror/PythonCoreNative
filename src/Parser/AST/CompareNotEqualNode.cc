6af92c9689ab85f9690ae23388cd69b072785d1a
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-23 15:42:03 +0200

    NotEqual access

--- src/Parser/AST/CompareNotEqualNode.cc
+++ src/Parser/AST/CompareNotEqualNode.cc
@@ -2,6 +2,7 @@
 #include <ast/CompareNotEqualNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 CompareNotEqualNode::CompareNotEqualNode(  
                             unsigned int start, unsigned int end, 
@@ -14,3 +15,19 @@ CompareNotEqualNode::CompareNotEqualNode(
     mOp1 = op1;
     mRight = right;
 }
+
+std::shared_ptr<ExpressionNode>  CompareNotEqualNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> CompareNotEqualNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> CompareNotEqualNode::GetRight()
+{
+    return mRight;
+}
+
