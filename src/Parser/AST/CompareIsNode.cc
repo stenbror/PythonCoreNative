e53054bf96628422d0a61c86083953b9194e0f31
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-23 14:46:53 +0200

    Is Access

--- src/Parser/AST/CompareIsNode.cc
+++ src/Parser/AST/CompareIsNode.cc
@@ -2,6 +2,7 @@
 #include <ast/CompareIsNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 CompareIsNode::CompareIsNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ CompareIsNode::CompareIsNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode>  CompareIsNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> CompareIsNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> CompareIsNode::GetRight()
+{
+    return mRight;
 }
