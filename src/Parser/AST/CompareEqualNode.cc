72fb103fafdf4ead2537777ffdb21a6ca86f51f2
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-23 13:49:57 +0200

    CompareEqual access

--- src/Parser/AST/CompareEqualNode.cc
+++ src/Parser/AST/CompareEqualNode.cc
@@ -2,6 +2,7 @@
 #include <ast/CompareEqualNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 CompareEqualNode::CompareEqualNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ CompareEqualNode::CompareEqualNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode>  CompareEqualNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> CompareEqualNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> CompareEqualNode::GetRight()
+{
+    return mRight;
 }
