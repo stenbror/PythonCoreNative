25efb0528c01aa7f79edbe1c012252d7f896eb84
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-23 15:18:30 +0200

    LessEqual access

--- src/Parser/AST/CompareLessEqualNode.cc
+++ src/Parser/AST/CompareLessEqualNode.cc
@@ -2,6 +2,7 @@
 #include <ast/CompareLessEqualNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 CompareLessEqualNode::CompareLessEqualNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ CompareLessEqualNode::CompareLessEqualNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode>  CompareLessEqualNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> CompareLessEqualNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> CompareLessEqualNode::GetRight()
+{
+    return mRight;
 }
