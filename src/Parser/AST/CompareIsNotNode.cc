89749e797c097bae2454e4e04789975123e86ce4
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-23 15:12:16 +0200

    IsNot access

--- src/Parser/AST/CompareIsNotNode.cc
+++ src/Parser/AST/CompareIsNotNode.cc
@@ -2,6 +2,7 @@
 #include <ast/CompareIsNotNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 CompareIsNotNode::CompareIsNotNode(  
                             unsigned int start, unsigned int end, 
@@ -15,4 +16,24 @@ CompareIsNotNode::CompareIsNotNode(
     mOp1 = op1;
     mOp2 = op2;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode>  CompareIsNotNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> CompareIsNotNode::GetOperator1()
+{
+    return mOp1;
+}
+
+std::shared_ptr<Token> CompareIsNotNode::GetOperator2()
+{
+    return mOp2;
+}
+
+std::shared_ptr<ExpressionNode> CompareIsNotNode::GetRight()
+{
+    return mRight;
 }
