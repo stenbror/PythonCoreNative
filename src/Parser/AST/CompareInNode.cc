3b180d009cff08a4031f7866957a11dc2cd8341c
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-23 14:43:48 +0200

    Compare In access

--- src/Parser/AST/CompareInNode.cc
+++ src/Parser/AST/CompareInNode.cc
@@ -2,6 +2,7 @@
 #include <ast/CompareInNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 CompareInNode::CompareInNode(  
                             unsigned int start, unsigned int end, 
@@ -14,3 +15,19 @@ CompareInNode::CompareInNode(
     mOp1 = op1;
     mRight = right;
 }
+
+std::shared_ptr<ExpressionNode>  CompareInNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> CompareInNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> CompareInNode::GetRight()
+{
+    return mRight;
+}
+
