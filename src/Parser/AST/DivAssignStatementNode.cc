80ab2dc4bdbd777585fec1442cc053fecb072904
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-23 17:04:53 +0200

    DivAssign access

--- src/Parser/AST/DivAssignStatementNode.cc
+++ src/Parser/AST/DivAssignStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/DivAssignStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 DivAssignStatementNode::DivAssignStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ DivAssignStatementNode::DivAssignStatementNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<Node> DivAssignStatementNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> DivAssignStatementNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> DivAssignStatementNode::GetRight()
+{
+    return mRight;
 }
