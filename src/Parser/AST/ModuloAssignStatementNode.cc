a24475a7686e830243213dcf94203eb265f6ff3e
stenbror <stenbror@hotmail.com>
2021-05-24 20:44:51 +0200

    ModuloAssign access

--- src/Parser/AST/ModuloAssignStatementNode.cc
+++ src/Parser/AST/ModuloAssignStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/ModuloAssignStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 ModuloAssignStatementNode::ModuloAssignStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ ModuloAssignStatementNode::ModuloAssignStatementNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<Node> ModuloAssignStatementNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> ModuloAssignStatementNode::GetOerator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> ModuloAssignStatementNode::GetRight()
+{
+    return mRight;
 }
