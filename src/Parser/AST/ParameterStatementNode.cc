aebea941471382721f3c865e8dafd5608e60c76d
stenbror <stenbror@hotmail.com>
2021-05-24 22:05:38 +0200

    Parameter access

--- src/Parser/AST/ParameterStatementNode.cc
+++ src/Parser/AST/ParameterStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/ParameterStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 ParameterStatementNode::ParameterStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ ParameterStatementNode::ParameterStatementNode(
     mRight = right;
     mOp1 = op1;
     mOp2 = op2;
+}
+
+std::shared_ptr<Token> ParameterStatementNode::GetOperator1()
+{
+    return mOp1;
+}
+
+std::shared_ptr<Token> ParameterStatementNode::GetOperator2()
+{
+    return mOp2;
+}
+
+std::shared_ptr<StatementNode> ParameterStatementNode::GetRight()
+{
+    return mRight;
 }
