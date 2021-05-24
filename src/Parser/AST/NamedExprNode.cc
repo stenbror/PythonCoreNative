be38160c6f63baa1f4b863a73a96cfd7c8b307ef
stenbror <stenbror@hotmail.com>
2021-05-24 21:16:11 +0200

    NamedExpr access

--- src/Parser/AST/NamedExprNode.cc
+++ src/Parser/AST/NamedExprNode.cc
@@ -2,6 +2,7 @@
 #include <ast/NamedExprNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 NamedExprNode::NamedExprNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ NamedExprNode::NamedExprNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode> NamedExprNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> NamedExprNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> NamedExprNode::GetRight()
+{
+    return mRight;
 }
