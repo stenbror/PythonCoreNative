92a1f92cc77fd720a737a55bb17cba7943e66fca
stenbror <stenbror@hotmail.com>
2021-05-24 20:57:39 +0200

    MulAssign access

--- src/Parser/AST/MulAssignStatementNode.cc
+++ src/Parser/AST/MulAssignStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/MulAssignStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 MulAssignStatementNode::MulAssignStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ MulAssignStatementNode::MulAssignStatementNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<Node> MulAssignStatementNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> MulAssignStatementNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> MulAssignStatementNode::GetRight()
+{
+    return mRight;
 }
