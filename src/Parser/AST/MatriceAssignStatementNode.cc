f45abc211bc59ded7a090da7ed08375b194572a4
stenbror <stenbror@hotmail.com>
2021-05-24 20:09:23 +0200

    MatriceAssign access

--- src/Parser/AST/MatriceAssignStatementNode.cc
+++ src/Parser/AST/MatriceAssignStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/MatriceAssignStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 MatriceAssignStatementNode::MatriceAssignStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,18 @@ MatriceAssignStatementNode::MatriceAssignStatementNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+std::shared_ptr<Node> MatriceAssignStatementNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> MatriceAssignStatementNode::GetOperartor()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> MatriceAssignStatementNode::GetRight()
+{
+    return mRight;
 }
