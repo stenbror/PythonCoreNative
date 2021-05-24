dedb159fa011de2317ebf5e8da7acee0e57ff336
stenbror <stenbror@hotmail.com>
2021-05-24 22:37:26 +0200

    Return access

--- src/Parser/AST/ReturnStatementNode.cc
+++ src/Parser/AST/ReturnStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/ReturnStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 ReturnStatementNode::ReturnStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -11,4 +12,14 @@ ReturnStatementNode::ReturnStatementNode(
 {
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<Token> ReturnStatementNode::GetOperator()
+{
+    return mOp1;
+} 
+
+std::shared_ptr<StatementNode> ReturnStatementNode::GetRight()
+{
+    return mRight;
 }
