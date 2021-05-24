6316327a6d9eed08f10a994f138922dfdb00d56b
stenbror <stenbror@hotmail.com>
2021-05-24 15:12:38 +0200

    Import Statement access

--- src/Parser/AST/ImportStatementNode.cc
+++ src/Parser/AST/ImportStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/ImportStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 ImportStatementNode::ImportStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -11,4 +12,14 @@ ImportStatementNode::ImportStatementNode(
 {
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<Token> ImportStatementNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<StatementNode> ImportStatementNode::GetRight()
+{
+    return mRight;
 }
