f2f03c2dad760b449b388ffc587b5c3b959ef9a0
stenbror <stenbror@hotmail.com>
2021-05-24 20:16:31 +0200

    Matrice access

--- src/Parser/AST/MatriceNode.cc
+++ src/Parser/AST/MatriceNode.cc
@@ -2,6 +2,7 @@
 #include <ast/MatriceNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 MatriceNode::MatriceNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ MatriceNode::MatriceNode(
     mOp1 = op1;
     mLeft = left;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode> MatriceNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> MatriceNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> MatriceNode::GetRight()
+{
+    return mRight;
 }
