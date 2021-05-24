51060fd7911839e8c0265f987108c48b117811fb
stenbror <stenbror@hotmail.com>
2021-05-24 22:22:53 +0200

    PowerAssign access

--- src/Parser/AST/PowerAssignStatementNode.cc
+++ src/Parser/AST/PowerAssignStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/PowerAssignStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 PowerAssignStatementNode::PowerAssignStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ PowerAssignStatementNode::PowerAssignStatementNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<Node> PowerAssignStatementNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> PowerAssignStatementNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> PowerAssignStatementNode::GetRight()
+{
+    return mRight;
 }
