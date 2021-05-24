17f511c868fd723521cdd9ab1e4bdd3b28141ee5
stenbror <stenbror@hotmail.com>
2021-05-24 14:02:06 +0200

    Global access

--- src/Parser/AST/GlobalStatementNode.cc
+++ src/Parser/AST/GlobalStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/GlobalStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 GlobalStatementNode::GlobalStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ GlobalStatementNode::GlobalStatementNode(
     mOp1 = op1;
     mNodes = nodes;
     mSeparators = separators;
+}
+
+std::shared_ptr<Token> GlobalStatementNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<std::vector<std::shared_ptr<NameToken>>> GlobalStatementNode::GetNodes()
+{
+    return mNodes;
+}
+
+std::shared_ptr<std::vector<std::shared_ptr<Token>>> GlobalStatementNode::GetSeparators()
+{
+    return mSeparators;
 }
