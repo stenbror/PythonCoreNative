f68c21bfd4744f94ce9a948318b9381d6a60354a
stenbror <stenbror@hotmail.com>
2021-05-24 15:55:11 +0200

    Lambda access

--- include/ast/LambdaNode.h
+++ include/ast/LambdaNode.h
@@ -18,6 +18,11 @@ namespace PythonCoreNative::RunTime::Parser::AST
                             std::shared_ptr<ExpressionNode> right
                         );
 
+            std::shared_ptr<ExpressionNode> GetLeft();
+            std::shared_ptr<Token> GetOperator1();
+            std::shared_ptr<Token> GetOperator2();
+            std::shared_ptr<ExpressionNode> GetRight();
+
         protected:
             std::shared_ptr<ExpressionNode> mLeft;
             std::shared_ptr<Token> mOp1;
--- src/Parser/AST/LambdaNode.cc
+++ src/Parser/AST/LambdaNode.cc
@@ -2,6 +2,7 @@
 #include <ast/LambdaNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 LambdaNode::LambdaNode(  
                             unsigned int start, unsigned int end, 
@@ -15,4 +16,24 @@ LambdaNode::LambdaNode(
     mOp1 = op1;
     mOp2 = op2;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode> LambdaNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> LambdaNode::GetOperator1()
+{
+    return mOp1;
+}
+
+std::shared_ptr<Token> LambdaNode::GetOperator2()
+{
+    return mOp2;
+}
+
+std::shared_ptr<ExpressionNode> LambdaNode::GetRight()
+{
+    return mRight;
 }
