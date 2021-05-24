8082f9ec53e80bd59041fe17e9f55a298755c957
stenbror <stenbror@hotmail.com>
2021-05-24 22:17:17 +0200

    Pluss access

--- src/Parser/AST/PlusNode.cc
+++ src/Parser/AST/PlusNode.cc
@@ -2,6 +2,7 @@
 #include <ast/PlusNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 PlusNode::PlusNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ PlusNode::PlusNode(
     mOp1 = op1;
     mLeft = left;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode> PlusNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> PlusNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> PlusNode::GetRight()
+{
+    return mRight;
 }
