6bb8b2d6c702bced41578b43970e59d54f0ed83a
stenbror <stenbror@hotmail.com>
2021-05-24 22:11:02 +0200

    PlusAssign access

--- src/Parser/AST/PlusAssignStatementNode.cc
+++ src/Parser/AST/PlusAssignStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/PlusAssignStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 PlusAssignStatementNode::PlusAssignStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ PlusAssignStatementNode::PlusAssignStatementNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<Node> PlusAssignStatementNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> PlusAssignStatementNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> PlusAssignStatementNode::GetRight()
+{
+    return mRight;
 }
