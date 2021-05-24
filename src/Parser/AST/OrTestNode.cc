d9f7cf6c3ee93d6a8741a8141cfaff670e4f86ab
stenbror <stenbror@hotmail.com>
2021-05-24 21:38:39 +0200

    OrTest access

--- src/Parser/AST/OrTestNode.cc
+++ src/Parser/AST/OrTestNode.cc
@@ -2,6 +2,7 @@
 #include <ast/OrTestNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 OrTestNode::OrTestNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ OrTestNode::OrTestNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode> OrTestNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> OrTestNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> OrTestNode::GetRight()
+{
+    return mRight;
 }
