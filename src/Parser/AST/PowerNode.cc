a823c0f7cbe6ecc8de3e58d1efdd237662e661bf
stenbror <stenbror@hotmail.com>
2021-05-24 22:25:46 +0200

    Power access

--- src/Parser/AST/PowerNode.cc
+++ src/Parser/AST/PowerNode.cc
@@ -2,6 +2,7 @@
 #include <ast/PowerNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 PowerNode::PowerNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ PowerNode::PowerNode(
     mOp1 = op1;
     mLeft = left;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode> PowerNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> PowerNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> PowerNode::GetRight()
+{
+    return mRight;
 }
