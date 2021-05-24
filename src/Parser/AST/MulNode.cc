95ad17cb595626e071a3859a0455d934c8b057e7
stenbror <stenbror@hotmail.com>
2021-05-24 21:12:25 +0200

    Mul access

--- src/Parser/AST/MulNode.cc
+++ src/Parser/AST/MulNode.cc
@@ -2,6 +2,7 @@
 #include <ast/MulNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 MulNode::MulNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ MulNode::MulNode(
     mOp1 = op1;
     mLeft = left;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode> MulNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> MulNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> MulNode::GetRight()
+{
+    return mRight;
 }
