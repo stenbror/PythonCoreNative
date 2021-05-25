524564ad7b14d8a96f3b71d2491dbd14511d6374
stenbror <stenbror@hotmail.com>
2021-05-25 21:46:24 +0200

    Test access

--- src/Parser/AST/TestNode.cc
+++ src/Parser/AST/TestNode.cc
@@ -2,6 +2,7 @@
 #include <ast/TestNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 TestNode::TestNode(  
                             unsigned int start, unsigned int end, 
@@ -17,4 +18,29 @@ TestNode::TestNode(
     mOp2 = op2;
     mRight = right;
     mNext = next;
+}
+
+std::shared_ptr<ExpressionNode> TestNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> TestNode::GetOperator1()
+{
+    return mOp1;
+}
+
+std::shared_ptr<Token> TestNode::GetOperator2()
+{
+    return mOp2;
+}
+
+std::shared_ptr<ExpressionNode> TestNode::GetRight()
+{
+    return mRight;
+}
+
+std::shared_ptr<ExpressionNode> TestNode::GetNext()
+{
+    return mNext;
 }
