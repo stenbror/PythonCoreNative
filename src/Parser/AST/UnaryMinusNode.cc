bce1131a650c04bc631f382b21ee7b5645fd3a55
stenbror <stenbror@hotmail.com>
2021-05-26 00:09:20 +0200

    UnaryMinus access

--- src/Parser/AST/UnaryMinusNode.cc
+++ src/Parser/AST/UnaryMinusNode.cc
@@ -2,6 +2,7 @@
 #include <ast/UnaryMinusNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 UnaryMinusNode::UnaryMinusNode(  
                             unsigned int start, unsigned int end, 
@@ -11,4 +12,14 @@ UnaryMinusNode::UnaryMinusNode(
 {
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<Token> UnaryMinusNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> UnaryMinusNode::GetRight()
+{
+    return mRight;
 }
