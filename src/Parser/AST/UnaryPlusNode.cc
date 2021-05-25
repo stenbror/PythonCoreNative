dff4c5e7c42b6328c6fc6af625109c37dedcd1c1
stenbror <stenbror@hotmail.com>
2021-05-26 00:11:50 +0200

    UnaryPlus access

--- src/Parser/AST/UnaryPlusNode.cc
+++ src/Parser/AST/UnaryPlusNode.cc
@@ -2,6 +2,7 @@
 #include <ast/UnaryPlusNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 UnaryPlusNode::UnaryPlusNode(  
                             unsigned int start, unsigned int end, 
@@ -11,4 +12,14 @@ UnaryPlusNode::UnaryPlusNode(
 {
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<Token> UnaryPlusNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> UnaryPlusNode::GetRight()
+{
+    return mRight;
 }
