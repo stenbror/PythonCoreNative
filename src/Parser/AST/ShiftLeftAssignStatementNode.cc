9595b426ad9a59aa0781d5447f35e2ecdc187c46
stenbror <stenbror@hotmail.com>
2021-05-25 15:55:01 +0200

    ShiftLeftAssign access

--- src/Parser/AST/ShiftLeftAssignStatementNode.cc
+++ src/Parser/AST/ShiftLeftAssignStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/ShiftLeftAssignStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 ShiftLeftAssignStatementNode::ShiftLeftAssignStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ ShiftLeftAssignStatementNode::ShiftLeftAssignStatementNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<Node> ShiftLeftAssignStatementNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> ShiftLeftAssignStatementNode::GetOperator()
+{
+    return mOp1;
+}
+
+std::shared_ptr<ExpressionNode> ShiftLeftAssignStatementNode::GetRight()
+{
+    return mRight;
 }
