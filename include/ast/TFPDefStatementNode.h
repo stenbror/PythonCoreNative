a305576a2fae6c26744c0de0a92ca1baa6702ee0
stenbror <stenbror@hotmail.com>
2021-05-25 23:11:37 +0200

    TFPDef access

--- include/ast/TFPDefStatementNode.h
+++ include/ast/TFPDefStatementNode.h
@@ -18,6 +18,10 @@ namespace PythonCoreNative::RunTime::Parser::AST
                             std::shared_ptr<ExpressionNode> mulNode
                         );
 
+            std::shared_ptr<Token> GetOperator1();
+            std::shared_ptr<Token> GetOperator2();
+            std::shared_ptr<ExpressionNode> GetRight();
+
         protected:
             std::shared_ptr<Token> mOp1;
             std::shared_ptr<Token> mOp2;
--- src/Parser/AST/TFPDefStatementNode.cc
+++ src/Parser/AST/TFPDefStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/TFPDefStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 TFPDefStatementNode::TFPDefStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,19 @@ TFPDefStatementNode::TFPDefStatementNode(
     mOp1 = op1;
     mOp2 = op2;
     mRight = right;
+}
+
+std::shared_ptr<Token> TFPDefStatementNode::GetOperator1()
+{
+    return mOp1;
+}
+
+std::shared_ptr<Token> TFPDefStatementNode::GetOperator2()
+{
+    return mOp2;
+}
+
+std::shared_ptr<ExpressionNode> TFPDefStatementNode::GetRight()
+{
+    return mRight;
 }
