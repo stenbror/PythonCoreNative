483234aa98c78826ab5fab6c6d00ae187aad2e8e
stenbror <stenbror@hotmail.com>
2021-05-26 18:39:20 +0200

    Finished CMakeLists upgrade for console app.

--- PythonCoreConsole/src/PythonCoreConsole.cc
+++ PythonCoreConsole/src/PythonCoreConsole.cc
@@ -1,7 +1,11 @@
 
 #include <PythonCoreParser.h>
 
+using namespace PythonCoreNative::RunTime::Parser;
+
 int main(int argc, char *argv[])
 {
+
+    auto buf = std::make_shared<SourceBuffer>( std::make_shared<std::wstring>(L"as ") );
     return 0;
 }
