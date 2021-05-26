629f4e926f8993834d927aa4c33d40f69330cea9
stenbror <stenbror@hotmail.com>
2021-05-26 19:00:24 +0200

    Console header

--- PythonCoreConsole/src/PythonCoreConsole.cc
+++ PythonCoreConsole/src/PythonCoreConsole.cc
@@ -1,11 +1,18 @@
 
 #include <PythonCoreParser.h>
 
+#include <iostream>
+
 using namespace PythonCoreNative::RunTime::Parser;
 
 int main(int argc, char *argv[])
 {
+    std::wcout << L"PythonCore ( Console ) \r\n";
+    std::wcout << L"Written by Richard Magnor Stenbro. https://github.com/stenbror/PythonCoreNative\r\n\r\n";
 
     auto buf = std::make_shared<SourceBuffer>( std::make_shared<std::wstring>(L"as ") );
+    auto lexer = std::make_shared<PythonCoreTokenizer>( 4, buf );
+
+    lexer->Advance();
     return 0;
 }
