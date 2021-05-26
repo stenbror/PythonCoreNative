aabfccf62fcb0d5e7e04c544fc0cbe1a16870a9b
stenbror <stenbror@hotmail.com>
2021-05-26 13:12:13 +0200

    WithItem access

--- src/Parser/AST/WithItemStatementNode.cc
+++ src/Parser/AST/WithItemStatementNode.cc
@@ -2,6 +2,7 @@
 #include <ast/WithItemStatementNode.h>
 
 using namespace PythonCoreNative::RunTime::Parser::AST;
+using namespace PythonCoreNative::RunTime::Parser;
 
 WithItemStatementNode::WithItemStatementNode(  
                             unsigned int start, unsigned int end, 
@@ -13,4 +14,18 @@ WithItemStatementNode::WithItemStatementNode(
     mLeft = left;
     mOp1 = op1;
     mRight = right;
+}
+
+std::shared_ptr<ExpressionNode> WithItemStatementNode::GetLeft()
+{
+    return mLeft;
+}
+
+std::shared_ptr<Token> WithItemStatementNode::GetOperator()
+{
+    return mOp1;
+}
+std::shared_ptr<ExpressionNode> WithItemStatementNode::GetRight()
+{
+    return mRight;
 }
