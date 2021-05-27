b0eb274ebc3afdf6be691cf9de4ff2af894e7c88
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-27 20:10:41 +0200

    UnitTest for None

--- tests/Parser/TestPythonCoreExpression.cc
+++ tests/Parser/TestPythonCoreExpression.cc
@@ -28,4 +28,23 @@ TEST_CASE( "Rule: Atom", "Parser - Expression rules" )
 
     }
 
+    SECTION( "Atom 'None'" )
+    {
+
+        auto sourceBuffer = std::make_shared<SourceBuffer>( std::make_shared<std::wstring>( L"None " ) );
+        auto lexer = std::make_shared<PythonCoreTokenizer>(4, sourceBuffer);
+        auto parser = std::make_shared<PythonCoreParser>(lexer);
+
+        auto root = std::static_pointer_cast<AST::EvalInputNode>( parser->ParseEvalInput() );
+
+        REQUIRE( root->GetNewlines()->size() == 0 );
+
+        auto node = std::static_pointer_cast<AST::AtomFalseNode>( root->GetRight() );
+
+        REQUIRE( node->GetOperator()->GetSymbolKind() == TokenKind::PyNone );
+        REQUIRE( node->GetOperator()->GetTokenStartPosition() == 0 );
+        REQUIRE( node->GetOperator()->GetTokenEndPosition() == 4 );
+
+    }
+
 }
