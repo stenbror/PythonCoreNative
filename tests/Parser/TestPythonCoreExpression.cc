684be045f091a60679688e2428ec1ece7d1a4610
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-28 16:16:49 +0200

    Started on UnitTests for term

--- tests/Parser/TestPythonCoreExpression.cc
+++ tests/Parser/TestPythonCoreExpression.cc
@@ -527,3 +527,38 @@ TEST_CASE( "Rule: Factor", "Parser - Expression rules" )
     }
 
 }
+
+
+TEST_CASE( "Rule: Term", "Parser - Expression rules" )
+{
+
+    SECTION( "a + b "  )
+    {
+
+        auto sourceBuffer = std::make_shared<SourceBuffer>( std::make_shared<std::wstring>( L"a * b " ) );
+        auto lexer = std::make_shared<PythonCoreTokenizer>(4, sourceBuffer);
+        auto parser = std::make_shared<PythonCoreParser>(lexer);
+
+        auto root = std::static_pointer_cast<AST::EvalInputNode>( parser->ParseEvalInput() );
+
+        REQUIRE( root->GetNewlines()->size() == 0 );
+
+        auto node = std::static_pointer_cast<AST::MulNode>( root->GetRight() );
+
+        REQUIRE( node->GetOperator()->GetSymbolKind() == TokenKind::PyMul );
+        REQUIRE( node->GetOperator()->GetTokenStartPosition() == 2 );
+        REQUIRE( node->GetOperator()->GetTokenEndPosition() == 3 );
+
+        auto left = std::static_pointer_cast<AST::AtomNameNode>( node->GetLeft() );
+        REQUIRE( left->GetNameText()->GetText()->compare(L"a") == 0 );
+
+        auto right = std::static_pointer_cast<AST::AtomNameNode>( node->GetRight() );
+        REQUIRE( right->GetNameText()->GetText()->compare(L"b") == 0 );
+
+        REQUIRE ( node->GetNodeStartPosition() == 0 ) ;  
+        REQUIRE ( node->GetNodeEndPosition() == 6 ) ; 
+
+
+    }
+
+}
