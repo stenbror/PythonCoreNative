21d6f3d5ee86a03eea20589c7dee29c9886f9596
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-28 21:50:32 +0200

    UnitTests for BitAnd passed.

--- tests/Parser/TestPythonCoreExpression.cc
+++ tests/Parser/TestPythonCoreExpression.cc
@@ -909,3 +909,74 @@ TEST_CASE( "Rule: Shift", "Parser - Expression rules" )
 
 }
 
+
+TEST_CASE( "Rule: AndExpr", "Parser - Expression rules" )
+{
+
+    SECTION( "a & b "  )
+    {
+
+        auto sourceBuffer = std::make_shared<SourceBuffer>( std::make_shared<std::wstring>( L"a & b " ) );
+        auto lexer = std::make_shared<PythonCoreTokenizer>(4, sourceBuffer);
+        auto parser = std::make_shared<PythonCoreParser>(lexer);
+
+        auto root = std::static_pointer_cast<AST::EvalInputNode>( parser->ParseEvalInput() );
+
+        REQUIRE( root->GetNewlines()->size() == 0 );
+
+        auto node = std::static_pointer_cast<AST::BitAndNode>( root->GetRight() );
+
+        REQUIRE( node->GetOperator()->GetSymbolKind() == TokenKind::PyBitAnd );
+        REQUIRE( node->GetOperator()->GetTokenStartPosition() == 2 );
+        REQUIRE( node->GetOperator()->GetTokenEndPosition() == 3 );
+
+        auto left = std::static_pointer_cast<AST::AtomNameNode>( node->GetLeft() );
+        REQUIRE( left->GetNameText()->GetText()->compare(L"a") == 0 );
+
+        auto right = std::static_pointer_cast<AST::AtomNameNode>( node->GetRight() );
+        REQUIRE( right->GetNameText()->GetText()->compare(L"b") == 0 );
+
+        REQUIRE ( node->GetNodeStartPosition() == 0 ) ;  
+        REQUIRE ( node->GetNodeEndPosition() == 6 ) ; 
+
+    }
+
+    SECTION( "a & b & c (recursive)"  )
+    {
+
+        auto sourceBuffer = std::make_shared<SourceBuffer>( std::make_shared<std::wstring>( L"a & b & c " ) );
+        auto lexer = std::make_shared<PythonCoreTokenizer>(4, sourceBuffer);
+        auto parser = std::make_shared<PythonCoreParser>(lexer);
+
+        auto root = std::static_pointer_cast<AST::EvalInputNode>( parser->ParseEvalInput() );
+
+        REQUIRE( root->GetNewlines()->size() == 0 );
+
+        auto node = std::static_pointer_cast<AST::BitAndNode>( root->GetRight() );
+
+        REQUIRE( node->GetOperator()->GetSymbolKind() == TokenKind::PyBitAnd );
+        REQUIRE( node->GetOperator()->GetTokenStartPosition() == 6 );
+        REQUIRE( node->GetOperator()->GetTokenEndPosition() == 7 );
+
+        auto node2 = std::static_pointer_cast<AST::BitAndNode>( node->GetLeft() );
+
+        auto leftFirst = std::static_pointer_cast<AST::AtomNameNode>( node2->GetLeft() );
+        REQUIRE( leftFirst->GetNameText()->GetText()->compare(L"a") == 0 );
+
+        REQUIRE( node2->GetOperator()->GetSymbolKind() == TokenKind::PyBitAnd) ;
+        REQUIRE( node2->GetOperator()->GetTokenStartPosition() == 2 ) ;
+        REQUIRE( node2->GetOperator()->GetTokenEndPosition() == 3 ) ;
+
+        auto rightFirst = std::static_pointer_cast<AST::AtomNameNode>( node2->GetRight() );
+        REQUIRE( rightFirst->GetNameText()->GetText()->compare(L"b") == 0 );
+
+        auto right = std::static_pointer_cast<AST::AtomNameNode>( node->GetRight() );
+        REQUIRE( right->GetNameText()->GetText()->compare(L"c") == 0 );
+
+        REQUIRE ( node->GetNodeStartPosition() == 0 ) ;  
+        REQUIRE ( node->GetNodeEndPosition() == 10 ) ; 
+
+    }
+
+}
+
