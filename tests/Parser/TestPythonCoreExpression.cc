9ce59071b27697c9258cae58697c91f60ee48340
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-29 13:51:40 +0200

    Second UnitTests for Lambda.

--- tests/Parser/TestPythonCoreExpression.cc
+++ tests/Parser/TestPythonCoreExpression.cc
@@ -1722,5 +1722,37 @@ TEST_CASE( "Rule: Lambda", "Parser - Expression rules" )
 
     }
 
+    SECTION( "lambda : a + 1 "  )
+    {
+
+        auto sourceBuffer = std::make_shared<SourceBuffer>( std::make_shared<std::wstring>( L"lambda : a + 1 " ) );
+        auto lexer = std::make_shared<PythonCoreTokenizer>(4, sourceBuffer);
+        auto parser = std::make_shared<PythonCoreParser>(lexer);
+
+        auto root = std::static_pointer_cast<AST::EvalInputNode>( parser->ParseEvalInput() );
+
+        REQUIRE( root->GetNewlines()->size() == 0 );
+
+        auto node = std::static_pointer_cast<AST::LambdaNode>( root->GetRight() );
+
+        REQUIRE( node->GetOperator1()->GetSymbolKind() == TokenKind::PyLambda );
+        REQUIRE( node->GetOperator1()->GetTokenStartPosition() == 0 );
+        REQUIRE( node->GetOperator1()->GetTokenEndPosition() == 6 );
+        
+        REQUIRE( node->GetLeft() == nullptr );
+
+        REQUIRE( node->GetOperator2()->GetSymbolKind() == TokenKind::PyColon );
+        REQUIRE( node->GetOperator2()->GetTokenStartPosition() == 7 );
+        REQUIRE( node->GetOperator2()->GetTokenEndPosition() == 8 );
+        
+        auto right = std::static_pointer_cast<AST::PlusNode>( node->GetRight() );
+        auto op1 = std::static_pointer_cast<AST::PlusNode>( right );
+        REQUIRE( op1->GetOperator()->GetSymbolKind() == TokenKind::PyPlus );
+
+        REQUIRE ( node->GetNodeStartPosition() == 0 ) ;  
+        REQUIRE ( node->GetNodeEndPosition() == 15 ) ; 
+
+    }
+
 }
 
