cf927d1fe136a60494305ffcf9269788f5ce45fd
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-30 16:57:31 +0200

    UnitTests for SubscriptList and Subscript passed.

--- tests/Parser/TestPythonCoreExpression.cc
+++ tests/Parser/TestPythonCoreExpression.cc
@@ -2937,5 +2937,28 @@ TEST_CASE( "Rule: SubscriptList and Subscript", "Parser - Expression rules" )
 
     }
 
+    SECTION( "SubscriptList failes" )
+    {
+
+        auto sourceBuffer = std::make_shared<SourceBuffer>( std::make_shared<std::wstring>( L"a[:,b " ) );
+        auto lexer = std::make_shared<PythonCoreTokenizer>(4, sourceBuffer);
+        auto parser = std::make_shared<PythonCoreParser>(lexer);
+
+        try
+        {
+            auto root = std::static_pointer_cast<AST::EvalInputNode>( parser->ParseEvalInput() );
+
+            REQUIRE( false );
+
+        }
+        catch( std::shared_ptr<SyntaxError> err )
+        {
+            REQUIRE(err->GetPosition() == 6);
+            REQUIRE(err->GetFailureSymbol()->GetSymbolKind() == TokenKind::EndOfFile);
+            REQUIRE(err->GetExceptionText()->compare(L"Expecting ']' in subscript!") == 0);
+        }
+        
+    }
+
 }
 
