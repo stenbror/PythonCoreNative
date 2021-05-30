ec8fae7552e46ed4480e04af85b63a035c8db7fe
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-05-30 14:24:02 +0200

    UnitTest for Subscript

--- tests/Parser/TestPythonCoreExpression.cc
+++ tests/Parser/TestPythonCoreExpression.cc
@@ -2712,5 +2712,52 @@ TEST_CASE( "Rule: SubscriptList and Subscript", "Parser - Expression rules" )
 
     }
 
+    SECTION( "a[::d] "  )
+    {
+
+        auto sourceBuffer = std::make_shared<SourceBuffer>( std::make_shared<std::wstring>( L"a[::d] " ) );
+        auto lexer = std::make_shared<PythonCoreTokenizer>(4, sourceBuffer);
+        auto parser = std::make_shared<PythonCoreParser>(lexer);
+
+        auto root = std::static_pointer_cast<AST::EvalInputNode>( parser->ParseEvalInput() );
+
+        REQUIRE( root->GetNewlines()->size() == 0 );
+
+        auto node = std::static_pointer_cast<AST::AtomExprNode>( root->GetRight() );
+
+        auto left = std::static_pointer_cast<AST::AtomNameNode>( node->GetLeft() );
+        REQUIRE( left->GetNameText()->GetText()->compare(L"a") == 0 );
+
+        auto trailers = node->GetRight();
+        REQUIRE( trailers->size() == 1 );
+
+        auto one = std::static_pointer_cast<AST::IndexNode>( trailers->at(0) );
+
+        REQUIRE( one->GetOperator1()->GetSymbolKind() == TokenKind::PyLeftBracket );
+        REQUIRE( one->GetOperator1()->GetTokenStartPosition() == 1 );
+        REQUIRE( one->GetOperator1()->GetTokenEndPosition() == 2 );
+
+        REQUIRE( one->GetOperator2()->GetSymbolKind() == TokenKind::PyRightBracket );
+        REQUIRE( one->GetOperator2()->GetTokenStartPosition() == 5 );
+        REQUIRE( one->GetOperator2()->GetTokenEndPosition() == 6 );
+
+        auto right = std::static_pointer_cast<AST::SubscriptNode>( one->GetRight() );
+        REQUIRE( right->GetLeft() == nullptr );
+        
+        REQUIRE( right->GetOperator1()->GetSymbolKind() == TokenKind::PyColon );
+
+        REQUIRE( right->GetRight() == nullptr );
+        
+        REQUIRE( right->GetOperator2()->GetSymbolKind() == TokenKind::PyColon );
+       
+        auto item3 = std::static_pointer_cast<AST::AtomNameNode>( right->GetNext() );
+        REQUIRE( item3->GetNameText()->GetText()->compare(L"d") == 0 );
+
+
+        REQUIRE ( node->GetNodeStartPosition() == 0 ) ;  
+        REQUIRE ( node->GetNodeEndPosition() == 7 ) ; 
+
+    }
+
 }
 
