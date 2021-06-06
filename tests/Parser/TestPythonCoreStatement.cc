488d8b6db981ee356ae749082d173bae054b8ad5
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-06-06 19:37:19 +0200

    UnitTest for failed elif

--- tests/Parser/TestPythonCoreStatement.cc
+++ tests/Parser/TestPythonCoreStatement.cc
@@ -269,5 +269,28 @@ TEST_CASE( "Rule: if/elif/else", "Parser - Statement rules" )
         
     }
 
+    SECTION( "Missing ':' in 'elif' failes" )
+    {
+
+        auto sourceBuffer = std::make_shared<SourceBuffer>( std::make_shared<std::wstring>( L"if a: pass\r\nelif b pass\r\n" ) );
+        auto lexer = std::make_shared<PythonCoreTokenizer>(4, sourceBuffer);
+        auto parser = std::make_shared<PythonCoreParser>(lexer);
+
+        try
+        {
+            auto root = std::static_pointer_cast<AST::EvalInputNode>( parser->ParseFileInput() );
+
+            REQUIRE( false );
+
+        }
+        catch( std::shared_ptr<SyntaxError> err )
+        {
+            REQUIRE(err->GetPosition() == 19);
+            REQUIRE(err->GetFailureSymbol()->GetSymbolKind() == TokenKind::PyPass);
+            REQUIRE(err->GetExceptionText()->compare(L"Missing ':' in 'elif' statement!") == 0);
+        }
+        
+    }
+
 }
 
