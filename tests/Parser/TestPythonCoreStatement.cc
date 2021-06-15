328f639059c282c6e015ca5e186d2328a3c6b2a8
stenbror <stenbror@hotmail.com>
2021-06-15 23:12:06 +0200

    Added first with test for missing colon.

--- tests/Parser/TestPythonCoreStatement.cc
+++ tests/Parser/TestPythonCoreStatement.cc
@@ -1030,5 +1030,28 @@ TEST_CASE( "Rule: with", "Parser - Statement rules" )
 
     }
 
+    SECTION( "Missing ':' in 'with' failes" )
+    {
+
+        auto sourceBuffer = std::make_shared<SourceBuffer>( std::make_shared<std::wstring>( L"with a as b pass\r\n" ) );
+        auto lexer = std::make_shared<PythonCoreTokenizer>(4, sourceBuffer);
+        auto parser = std::make_shared<PythonCoreParser>(lexer);
+
+        try
+        {
+            auto root = std::static_pointer_cast<AST::EvalInputNode>( parser->ParseFileInput() );
+
+            REQUIRE( false );
+
+        }
+        catch( std::shared_ptr<SyntaxError> err )
+        {
+            REQUIRE(err->GetPosition() == 12 );
+            REQUIRE(err->GetFailureSymbol()->GetSymbolKind() == TokenKind::PyPass);
+            REQUIRE(err->GetExceptionText()->compare(L"Missing ':' in 'with' statement!") == 0);
+        }
+        
+    }
+
 }
 
